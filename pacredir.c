a0adebb936a255477ca48601ce3bb71550dd4187
Christian Hesse <mail@eworm.de>
2025-07-18 11:45:36 +0200

    show finds in status dump

--- pacredir.c
+++ pacredir.c
@@ -879,14 +879,15 @@ static void sigusr_callback(int signal) {
 		write_log(stdout, " (none)\n");
 	while (hosts_ptr->host != NULL) {
 		if (hosts_ptr->badcount > 0)
-			write_log(stdout, " -> %s (%s, %s, port: %d, bad count: %d)\n",
+			write_log(stdout, " -> %s (%s, %s, port: %d, finds: %d, bad count: %d)\n",
 				hosts_ptr->host, hosts_ptr->mdns ? "mdns" : "static",
 				hosts_ptr->online ? "online" : "offline", hosts_ptr->port,
-				hosts_ptr->badcount);
+				hosts_ptr->finds, hosts_ptr->badcount);
 		else
-			write_log(stdout, " -> %s (%s, %s, port: %d)\n",
+			write_log(stdout, " -> %s (%s, %s, port: %d, finds: %d)\n",
 				hosts_ptr->host, hosts_ptr->mdns ? "mdns" : "static",
-				hosts_ptr->online ? "online" : "offline", hosts_ptr->port);
+				hosts_ptr->online ? "online" : "offline", hosts_ptr->port,
+				hosts_ptr->finds);
 
 		hosts_ptr = hosts_ptr->next;
 	}
